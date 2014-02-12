8536275490647d17b831e88c3e79c158270c0f85
briceorbryce <briceorbryce@gmail.com>
2014-02-12 01:30:10 -1000

    programming

--- hw3/ex1.asm
+++ hw3/ex1.asm
@@ -4,6 +4,14 @@
 %include "asm_io.inc"
 
 segment .data	
+	msg1	db	"Enter a character: ", 0
+	msg2	db	"Enter a integer: ", 0
+	msg3	db	"The character entered was: ", 0
+	msg4	db	"The integer entered was: ", 0
+
+segment .bss
+	chartr	resd	1	; holds the char the user puts
+	integer	resd	1	; holds the integer the user puts
 
 segment .text
 	global asm_main
@@ -12,7 +20,27 @@ asm_main:
 	pusha
 ; start
 
+	mov	eax, msg1	; print msg1
+	call	print_string
+	call	print_nl
 
+	call	read_char	; user entered char - stored in EAX
+	mov	[chartr], eax	; store in mem
+	;mov	eax, [chartr]
+	;call	print_char
+	;call	print_nl
+
+	mov	eax, msg2	; print msg2
+	call	print_string
+	call	print_nl
+
+	mov	eax, msg3	; print msg3
+	call	print_string
+	call	print_nl
+
+	mov	eax, msg4	; print msg4
+	call	print_string
+	call	print_nl
 
 ; end
 	popa
--- template.asm
+++ template.asm
@@ -6,6 +6,11 @@ segment .data
 ; bytes	dd	06C697665h	; "live"
 ; end	db	0	       	; null
 
+segment .bss
+;	integer1	rsd	1	
+;	integer2	rsd	1
+;	result		rsd	1
+
 segment .text
 	global asm_main
 asm_main:
