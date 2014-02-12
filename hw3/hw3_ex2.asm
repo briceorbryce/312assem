1e457425472679f9a076fc9be06b4adf0ba843a3
briceorbryce <briceorbryce@gmail.com>
2014-02-12 02:45:06 -1000

    fin

---
+++ hw3/hw3_ex2.asm
@@ -0,0 +1,22 @@
+; ex2
+; briceorbryce
+
+%include "asm_io.inc"
+
+segment .data	
+
+segment .text
+	global asm_main
+asm_main:
+	enter	0,0
+	pusha
+; start
+
+
+
+; end
+	popa
+	mov	eax, 0
+	leave
+	ret
+
