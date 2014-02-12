89f04a8216ff294e5a95f3fc5c50f5ffc7fade89
briceorbryce <briceorbryce@gmail.com>
2014-02-11 17:33:14 -1000

    homework files added from the template

---
+++ hw3/ex2.asm
@@ -0,0 +1,25 @@
+; This simple program demonstrate that the processor
+; uses Little Endian
+
+%include "asm_io.inc"
+
+segment .data	
+; bytes	dd	06C697665h	; "live"
+; end	db	0	       	; null
+
+segment .text
+	global asm_main
+asm_main:
+	enter		0,0
+	pusha
+; start
+
+
+
+; end
+	pusha
+	popa
+	mov		eax, 0
+	leave
+	ret
+
