a436e56ae2d2c0efc9a3abb5c02a1e6234677667
briceorbryce <briceorbryce@gmail.com>
2014-02-13 01:41:24 -1000

    fin

--- hw3/hw3_ex2.asm
+++ hw3/hw3_ex2.asm
@@ -4,6 +4,13 @@
 %include "asm_io.inc"
 
 segment .data	
+	prompt	db	"Enter a 5-character string: ", 0
+	str1	db	"String #1: ", 0
+	str2	db	"String #2: ", 0
+
+segment .bss
+	userstr	resq	1	; holds the user input - 16 bytes
+	backwrds resq	1	; holds the backwards string - 16 bytes
 
 segment .text
 	global asm_main
@@ -11,9 +18,51 @@ asm_main:
 	enter	0,0
 	pusha
 ; start
+	mov	ebx, userstr	; mov addr of userstr to ebx
+	mov	ecx, backwrds	; mov addr of backwrds to ecx
+	mov	BYTE [ecx + 5], 0	; terminate string that hasn't been created yet
+	mov	eax, prompt	; print "enter 5 char str"
+	call	print_string
 
+	call	read_char	; get char of user input
+	mov	[ebx], al	; mov to ebx and userstr
+	mov	[ecx + 4], al	; mov to ecx and 4 bytes ahead
+	
+	call	read_char	
+	mov	[ebx + 1], al	; save char in next byte
+	mov	[ecx + 3], al	; save char in previous byte
+	
+	call	read_char
+	mov	[ebx + 2], al
+	mov	[ecx + 2], al
+	
+	call	read_char
+	mov	[ebx + 3], al
+	mov	[ecx + 1], al
+	
+	call	read_char
+	mov	[ebx + 4], al
+	mov	[ecx], al
 
+	mov	BYTE [ebx + 5], 0	; terminate with null byte
 
+	mov	eax, str1	; print "string 1: "
+	call	print_string
+	mov	eax, ecx	; print that string the user entered backwards
+	call	print_string
+	call	print_nl
+	
+	sub	BYTE [ebx], 32
+	sub	BYTE [ebx + 1], 32
+	sub	BYTE [ebx + 2], 32
+	sub	BYTE [ebx + 3], 32
+	sub	BYTE [ebx + 4], 32
+	
+	mov	eax, str2	; print "string2:"
+	call	print_string
+	mov	eax, ebx
+	call	print_string	
+	call	print_nl
 ; end
 	popa
 	mov	eax, 0
