a0775935e9a92a21444470aa8a916fe658a4a8fc
briceorbryce <briceorbryce@gmail.com>
2014-03-02 01:35:31 -1000

    updated gitignore; need to do some stuff with homework 4 and the stack...

--- hw4/ex2.asm
+++ hw4/ex2.asm
@@ -1,37 +1,73 @@
-section .data	; data segment
-msg     db      "Hello, world!", 0x0a, 0x   ; the string and newline char
-section	.text	; Text segment
-global _start	; Default entry for ELF linking
+%include "asm_io.inc"
 
-_start:
+section .data	; data segment
+
+section	.text	; Text segment
+global asm_main	; Default entry for ELF linking
+
+asm_main:
+enter 0,0
+pusha
+call print_nl
 
 ; prologue
-push ebp
-mov ebp, esp
-mov ebp, esp    ; move stack ptr to ebp
+;push ebp
+;mov ebp, esp
+;mov ebp, esp    ; move stack ptr to ebp
 
-sub esp, 0xC
-mov DWORD [ebp-2], 0x8FF0
-mov DWORD [ebp-2], 0xA026
-mov DWORD [ebp-2], 0x6043
-mov DWORD [ebp-2], 0x7ABC
-mov DWORD [ebp-1], 0xF3
-mov DWORD [ebp-1], 0x0D
-mov DWORD [ebp-1], 0xE5
-mov DWORD [ebp-1], 0x03
+;sub esp, 0xC
+;mov WORD [ebp-2], 0x8FF0
+;mov WORD [ebp-4], 0xA026
+;mov WORD [ebp-6], 0x6043
+;mov WORD [ebp-8], 0x7ABC
+;mov BYTE [ebp-9], 0xF3
+;mov BYTE [ebp-0xa], 0x0D
+;mov BYTE [ebp-0xb], 0xE5
+;mov BYTE [ebp-0xc], 0x03
+
+;mov eax, [ebp-2]
+;mov ebx, [ebp-4]
+;add eax, ebx
+;call print_int
+;call print_nl
+
+xor eax, eax
+xor ebx, ebx
+mov ax, 0x8ff0
+mov bx, 0xa026
+add ax, bx
+call print_int
+call print_nl
+
+xor eax, eax
+xor ebx, ebx
+mov ax, 0x6043
+mov bx, 0x7ABC
+add ax, bx
+call print_int
+call print_nl
 
 
-mov eax, 4	; put 4 into eax since write is syscall #4
-mov ebx, 1	; put 1 into ebx since stdout is 1
-mov ecx, msg
-mov edx, 14
-int 0x80	; call the kernel to make the system call happen
+xor eax, eax
+xor ebx, ebx
+mov al, 0xF3
+mov bl, 0x0D
+add al, bl
+call print_int
+call print_nl
+
+mov al, 0xE5
+mov bl, 0x03
+add al, bl
+call print_int
+call print_nl
+
 
 ; epilogue
-mov esp, ebp
-pop ebp
+;mov esp, ebp
+;pop ebp
 
-; SYSCALL: exit(0)
-mov eax, 1	; put 1 into eax, since exit is syscall #1
-mov ebx, 0	; exit with success
-int 0x80	; do the syscall
+popa
+mov eax, 0
+leave
+ret
