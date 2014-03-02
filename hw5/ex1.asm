e55c1dd3ecb4d1a0a0eceac4e04cc190ae7c8e18
briceorbryce <briceorbryce@gmail.com>
2014-03-02 03:56:42 -1000

    fin

--- hw5/ex1.asm
+++ hw5/ex1.asm
@@ -29,48 +29,66 @@ asm_main:
 ;	sub esp, 0x4	; set up stack for local variable
 ;	mov DWORD [ebp-4], 0x0
 ; start
-	
-	;loop_start:
+	loop_start:
 	mov	eax, msg1	; print "enter int"
 	call	print_string
 	call	read_int
-	call	print_nl
 	
 	mov DWORD [userEnt], eax	; save input
-	cmp DWORD [userEnt], 0	; user < 0 ? jmp : go on
-	jl	exit_loop	; jmp if user entered neg
+	cmp DWORD [userEnt], 0		; user < 0 ? 
+	jl	exit_loop		; jmp if user entered neg
 	
-	cmp DWORD [userEnt], 0x20
-	jz	entered_space	; jmp if user entered 32(space)
+	cmp DWORD [userEnt], 0x20	; user == 32 (space) ?
+	jz	entered_space		; jmp if true
 	
+	cmp DWORD [userEnt], 0x100 	; user >= 256 ?
+	jge	not_ascii		; jmp if true
 	
+	cmp DWORD [userEnt], 0x80	; user >= 128?
+	jge	non_ext			; jmp if true
+	
+	cmp DWORD [userEnt], 0x3A	; user >= 58
+	jge	non_ext			; jmp if true
+	
+	cmp DWORD [userEnt], 0x30	; user >= 48
+	jge	entered_digit		; jmp if true
+	
+	cmp DWORD [userEnt], 0x21	; user >= 33
+	jge	non_ext			; jmp if true
+	
+	; user entered 0-31
+	jmp	non_ext
 	
 	entered_space:
 	mov	eax, msg2
 	call	print_string
 	call	print_nl
-	;jmp	loop_start
-	jmp	exit_loop
-		
+	jmp	loop_start
+	
 	entered_digit:
 	mov	eax, msg3
 	call	print_string
 	call	print_nl
-	;jmp	loop_start
+	jmp	loop_start
 	
 	non_ext:
 	mov	eax, msg4
 	call	print_string
 	call	print_nl
-	;jmp	loop_start
-
+	jmp	loop_start
+	
 	some_ext:
 	mov	eax, msg5
 	call	print_string
 	call	print_nl
-	;jmp	loop_start
-
-	; jmp loop_start
+	jmp	loop_start
+	
+	not_ascii:
+	mov	eax, msg6
+	call	print_string
+	call	print_nl
+	jmp	loop_start
+	
 	exit_loop:
 ; epilogue
 ;	mov esp, ebp
