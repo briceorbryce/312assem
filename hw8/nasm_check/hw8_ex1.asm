f6e5fd733f0cd36c98b3bb3462f4c0f5a5d91e47
briceorbryce <briceorbryce@gmail.com>
2014-04-19 06:23:35 -1000

    started hw8

---
+++ hw8/nasm_check/hw8_ex1.asm
@@ -0,0 +1,57 @@
+; Solution for Programming Assignment #7
+
+%include "asm_io.inc"
+
+segment .bss
+	Array	resd	10
+
+segment .text
+        global  asm_main
+
+asm_main:
+	enter	0,0			; setup
+	pusha				; setup
+
+	; Call function inputArray
+	push 	dword 10
+	push	Array
+	call	inputArray
+	add	esp, 8
+
+	; Call function printArray
+	push	dword 10
+	push	Array
+	call	printArray
+	add	esp, 8
+
+	popa				; cleanup
+	mov	eax, 0			; cleanup
+	leave				; cleanup
+	ret				; cleanup
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; DO NOT MODIFY ANYTHING ABOVE THIS LINE ;;;;;;;;;;;;;;;
+
+;; To implement: function inputArray
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+segment .data
+
+segment .bss
+
+segment .text
+
+inputArray:
+
+
+;; To implement: function printArray
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+segment .data
+
+segment .bss
+
+segment .text
+
+printArray:
+
